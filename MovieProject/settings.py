c10a2f2d629b3499f126b3d8589d2cb91f328de7
SowmiyaKM <sowmiyakmk@gmail.com>
2026-06-28 23:50:15 +0530

    same change email

--- MovieProject/settings.py
+++ MovieProject/settings.py
@@ -129,19 +129,12 @@ CACHES = {
 }
 
 # Email Configuration
-
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-
 EMAIL_HOST = "smtp.gmail.com"
-
 EMAIL_PORT = 587
-
 EMAIL_USE_TLS = True
-
 EMAIL_HOST_USER = "sowmiyakmk@gmail.com"
-
-EMAIL_HOST_PASSWORD = "gwwpuucjdxtcebor"
-
+EMAIL_HOST_PASSWORD = "fofyvtcusjrrhlig"
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 LOGGING = {
